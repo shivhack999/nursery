f573800ea3260bb8aaa916dfdefe5a923be1e811
solarShiv <solarshiv9@gmail.com>
2025-09-12 18:00:46 +0530

    a

--- backend/routers/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const loginRouter = require("./auth.router.js");
-
-router.use("/auth", loginRouter);
-
-module.exports = router;
